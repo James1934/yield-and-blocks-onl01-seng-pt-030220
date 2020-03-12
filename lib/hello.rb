471735772b2b40f0620e3f476c0fc0a8fbc2b797
Bartholomew Fountain <baertholomew@gmail.com>
2020-03-12 05:17:27 +0000

    Automatically backed up by Learn

---
+++ lib/hello.rb
@@ -0,0 +1,17 @@
+def hello_t(array)
+  if block_given?
+    i = 0
+
+
+end	    while i < array.length
+
+      yield(array[i])
+    i = i + 1
+    end
+
+
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
